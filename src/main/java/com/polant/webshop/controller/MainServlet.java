486f54133ac3a0abcf492a3eba94d281d73709f5
Polant <anton.poltoratskii@gmail.com>
2016-03-06 19:12:32 +0200

    configure and deploy tomcat

---
+++ src/main/java/com/polant/webshop/controller/MainServlet.java
@@ -0,0 +1,22 @@
+package com.polant.webshop.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Antony on 06.03.2016.
+ */
+@WebServlet("/")
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.print("<h1>Hello Servlet</h1>");
+    }
+}
