c7832ca349d9d6ac2f08707511caedc60d63e473
Polant <anton.poltoratskii@gmail.com>
2016-03-12 14:26:34 +0200

    implements validation in RegistrationServlet.java

--- src/main/java/com/polant/webshop/controller/RegistrationServlet.java
+++ src/main/java/com/polant/webshop/controller/RegistrationServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 /**
  * Данный сервлет обрабатывает регистрацию пользователя на сайте.
@@ -26,29 +27,66 @@ public class RegistrationServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        registerUser(req, resp);
+    }
+
+    private void registerUser(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         String login = req.getParameter("login");
         String[] password = req.getParameterValues("password");
         String email = req.getParameter("email");
 
-        if (!password[0].equals(password[1])){
-            //пароли должны совпадать.
-            passwordsEqualsError(req, resp);
-            return;
+        boolean validLogin = true;
+        boolean validPassword = true;
+        boolean validEmail = true;
+
+        if (login.equals("")){
+            req.setAttribute("EMPTY_LOGIN", true);
+            validLogin = false;
         }
-        User user = storage.registerUser(login, password[0], email);
-        if (user != null){
-            //Удачная регистрация.
-            openSession(req, resp, user);
-        }else {
-            //Если регистрация прошла неудачно (такой логин уже существует).
-            loginAlreadyExists(req, resp);
+
+        if (password[0].equals("")){
+            req.setAttribute("EMPTY_PASSWORD", true);
+            validPassword = false;
         }
+        else if (!password[0].equals(password[1])){//пароли должны совпадать.
+            req.setAttribute("PASSWORDS_NOT_EQUALS", true);
+            validPassword = false;
+        }
+
+        if (!email.equals("")){
+            if (!Pattern.matches("^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$", email)){
+                req.setAttribute("EMAIL_NOT_VALID", true);
+                validEmail = false;
+            }
+        }
+
+        if (validEmail && validPassword && validLogin) {
+            User user = storage.registerUser(login, password[0], email);
+            if (user != null) {
+                //Удачная регистрация.
+                openSession(req, resp, user);
+                return;
+            } else {
+                //Если такой логин уже существует.
+                req.setAttribute("LOGIN_ALREADY_EXISTS", true);
+                validLogin = false;
+            }
+        }
+        //Если пользователь ввел невалидные данные, то он не покидает страницу регистрации.
+        backToRegistrationPage(req, resp, login, password[0], email, validLogin, validPassword, validEmail);
     }
 
-    private void passwordsEqualsError(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("PASSWORDS_NOT_EQUALS", true);
+    private void backToRegistrationPage(HttpServletRequest req, HttpServletResponse resp, String login, String password,
+                                        String email, boolean validLogin, boolean validPassword, boolean validEmail) throws ServletException, IOException {
+
+        if (validLogin) req.setAttribute("login", login);
+        if (validPassword) req.setAttribute("password", password);
+        if (validEmail) req.setAttribute("email", email);
+
         req.getRequestDispatcher("/view/registration.jsp").forward(req, resp);
     }
+
 
     private void openSession(HttpServletRequest req, HttpServletResponse resp, User user) throws IOException {
         HttpSession newSession = req.getSession(true);
@@ -56,10 +94,5 @@ public class RegistrationServlet extends HttpServlet {
         newSession.setAttribute("user_id", user.getId());
 
         resp.sendRedirect(String.format("%s%s", req.getContextPath(), "/"));
-    }
-
-    private void loginAlreadyExists(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("LOGIN_ALREADY_EXISTS", true);
-        req.getRequestDispatcher("/view/registration.jsp").forward(req, resp);
     }
 }
--- src/main/webapp/view/registration.jsp
+++ src/main/webapp/view/registration.jsp
@@ -23,22 +23,31 @@
 
         <p>Обязательные поля обозначены *зведочкой : </p>
 
-        <c:if test="${not empty PASSWORDS_NOT_EQUALS}">
-            <p class="error_message">Пароли должны совпадать!</p>
+        <c:if test="${not empty EMPTY_LOGIN}">
+            <p class="error_message">Логин не может быть пустым!</p>
         </c:if>
         <c:if test="${not empty LOGIN_ALREADY_EXISTS}">
             <p class="error_message">Данный логин уже существует!</p>
         </c:if>
+        <c:if test="${not empty EMPTY_PASSWORD}">
+            <p class="error_message">Пароль не может быть пустым!</p>
+        </c:if>
+        <c:if test="${not empty PASSWORDS_NOT_EQUALS}">
+            <p class="error_message">Пароли должны совпадать!</p>
+        </c:if>
+        <c:if test="${not empty EMAIL_NOT_VALID}">
+            <p class="error_message">Почта имеет неверный формат!</p>
+        </c:if>
 
         <div style="margin: 0px auto; text-align: center">
             <label for="login">*Логин : </label><br />
-            <input type="text" id="login" name="login" value="" placeholder="login"/><br /><br />
+            <input type="text" id="login" name="login" value="${login}" placeholder="login"/><br /><br />
             <label for="login">Email : </label><br />
-            <input type="text" id="email" name="email" value="" placeholder="your.email@mail.com"/><br /><br />
+            <input type="text" id="email" name="email" value="${email}" placeholder="your.email@mail.com"/><br /><br />
             <label for="passwordFirst">*Введите пароль : </label><br />
-            <input type="password" id="passwordFirst" name="password" value="" placeholder="password"/><br /><br />
+            <input type="password" id="passwordFirst" name="password" value="${password}" placeholder="password"/><br /><br />
             <label for="passwordSecond">*Подтвердите пароль: </label><br />
-            <input type="password" id="passwordSecond" name="password" value="" placeholder="password"/><br /><br />
+            <input type="password" id="passwordSecond" name="password" value="${password}" placeholder="password"/><br /><br />
 
             <input type="submit" value="Зарегистрироваться" />
             <input type="reset" value="Сбросить" />
